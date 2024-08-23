5b3028345fb3fa45921f6617be54873047389982
ajaytidke28297 <ajaytidke28297@gmail.com>
2024-08-23 18:31:54 +0530

    add search and movie tabs

---
+++ src/MovieList.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { tempMovieData } from "./Main";
+import Movie from "./Movie";
+
+function MovieList() {
+  const [movies, setMovies] = useState(tempMovieData);
+
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+export default MovieList;
---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "./Logo";
+import NumResults from "./NumResults";
+import Search from "./Search";
+
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <NumResults />
+    </nav>
+  );
+}
+
+export default NavBar;
